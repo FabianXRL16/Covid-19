0fd988fdd0e54e6bdc8360a2e50962d7627e6986
Fabian Pacherres <fabianpacherres@gmail.com>
2021-10-08 23:25:15 -0500

    Principal structure

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert("hola")
+// const URL = "https://api.covid19api.com/summary";
+
+// const HTMLResponse = document.querySelector('#app');
+// const ul = document.createElement('ul')
+
+// fetch(URL)
+//     .then((response) => {
+//         response.json()
+//         console.log(response)
+//     })
+//     // .then((Countries) => {
+//     //     Countries.forEach(country => {
+//     //         let elem = document.createElement('li');
+//     //         elem.appendChild(
+//     //             document.createTextNode(`${country.Country}`)
+//     //         );
+//     //         ul.appendChild(elem)
+//     //     });
+//     //     HTMLResponse.appendChild(ul)
+//     // })
